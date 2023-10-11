3c705192736c160cd6ebf7399c69a9883d0cc729
aaryansatpal <147051114+aaryansatpal@users.noreply.github.com>
2023-10-11 12:39:45 +0530

    added sixth line in index.js in branchone

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log('second line');
 console.log('third line');
 console.log('fixed error and fourth line added in branchone');
 console.log('fifth line. to learn push');
+console.log('sixth line. added from github in branchone');
